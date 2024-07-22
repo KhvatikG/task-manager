8a509bd0b85dd9715be92dff1d284e75b7c4fd7f
Garik <khvatik.igor@gmail.com>
2024-07-22 15:09:40 +0300

    Views: add perform_create to TaskAPIView

--- taskmanager/checklist/views.py
+++ taskmanager/checklist/views.py
@@ -1,14 +1,42 @@
 from rest_framework import generics, viewsets, permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.serializers import ModelSerializer
 
-from checklist.models import Task, Department, CheckList
-from checklist.serializers import TaskSerializer, DepartmentSerializer, CheckListSerializer
+
+from checklist.models import Task, Department, CheckList, TaskPhoto
+from checklist.serializers import (
+    TaskSerializer,
+    DepartmentSerializer,
+    CheckListSerializer,
+    TaskPhotoSerializer,
+)
+
+# Вьюшка для получения/добавления пользователей
+"""
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]  # Ограничиваем доступ только для администраторов
+"""
 
 
 class TaskAPIView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Сначала сохраняем основную задачу
+        task = serializer.save()
+
+        # Затем получаем фотографии, если они были переданы
+        photos_data = self.request.data.get('example_photos', [])
+
+        for photo_data in photos_data:
+            # Валидируем и создаём фото
+            photo_serializer = TaskPhotoSerializer(data=photo_data)
+            if photo_serializer.is_valid(raise_exception=True):
+                photo_serializer.save(task=task)
 
 
 class DepartmentAPIView(viewsets.ModelViewSet):
@@ -24,3 +52,32 @@ class CheckListAPIView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer: ModelSerializer):
         serializer.save(created_by=self.request.user)
+
+    """
+    Фильтрация
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['department', 'is_published']
+    search_fields = ['title']
+    ordering_fields = ['time_create', 'title']
+    """
+
+    # Метод для проверки прав доступа к методам вьюшки например Assignment
+    """
+
+    def get_permissions(self):
+        /"/"/"
+        В зависимости от метода (POST, PUT, DELETE) 
+        устанавливаем разные разрешения.
+        /"/"/"
+        if self.request.method in permissions.SAFE_METHODS:
+            # Для GET, HEAD, OPTIONS разрешаем всем аутентифицированным пользователям
+            return [permissions.IsAuthenticated()]
+        else:
+            # Для POST, PUT, DELETE требуем права "create_checklistassignment"
+            return [permissions.IsAuthenticated(), permissions.IsAdminUser()]
+    
+    *  В  permission_classes  мы  указываем,  что  доступ  к  CheckListAssignmentViewSet  имеют  только  авторизованные  пользователи.
+    *  get_permissions  переопределяет  метод,  который  устанавливает  разные  permissions  в  зависимости  от  метода  HTTP-запроса.
+    *  В  get_permissions  мы  проверяем,  является  ли  метод  SAFE_METHODS  (GET, HEAD, OPTIONS),  и  если  да,  то  разрешаем  доступ  всем  авторизованным  пользователям.  
+    *  Для  POST,  PUT,  DELETE  методов  мы  требуем  права  IsAdminUser,  чтобы  только  администраторы  могли  создавать,  изменять  и  удалять  назначения.
+    """
