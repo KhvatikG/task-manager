3528106ae36b6b5df1df7db6dac68cfbbeafade9
Garik <khvatik.igor@gmail.com>
2024-08-02 14:33:41 +0300

    add MEDIA_URL and MEDIA_ROOT for work with photo

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -124,12 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"""
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'tests/media')
-else:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-"""
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGGING = {
     'version': 1,
